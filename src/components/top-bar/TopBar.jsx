aaffc403a5d08d0cf0868d19ae1fae887c93036a
David Zuluaga <dzuluagac@abatechnology.com>
2025-04-01 14:38:08 -0500

    correction of the top bar component

--- src/components/top-bar/TopBar.jsx
+++ src/components/top-bar/TopBar.jsx
@@ -1,26 +1,26 @@
 import logo from '../../assets/images/logo.svg';
 import logoCart from '../../assets/images/icon-cart.svg';
 import avatar from '../../assets/images/image-avatar.png';
-import './TopBar.css';
+import styles from './TopBar.module.css';
 
 const TopBar = () => {
     return (
         <>
-        <div className='top__bar'>
-            <div className='top__bar--nav'>
-                <img className='top__bar--logo' src={logo} />
-                <button className='nav__button'>Collections</button>
-                <button className='nav__button'>Men</button>
-                <button className='nav__button'>Women</button>
-                <button className='nav__button'>About</button>
-                <button className='nav__button'>Contact</button>
+        <div className={styles['top__bar']}> 
+            <div className={styles['top__bar--nav']}>
+                <img className={styles['top__bar--logo']} src={logo} />
+                <button className={styles['nav__button']}>Collections</button>
+                <button className={styles['nav__button']}>Men</button>
+                <button className={styles['nav__button']}>Women</button>
+                <button className={styles['nav__button']}>About</button>
+                <button className={styles['nav__button']}>Contact</button>
             </div>
-            <div className='top__bar--cart'>
-                <img className='top__bar__cart' src={logoCart} />
-                <img className='top__bar__avatar' src={avatar} />
+            <div className={styles['top__bar--cart']}>
+                <img className={styles['top__bar__cart']} src={logoCart} />
+                <img className={styles['top__bar__avatar']} src={avatar} />
             </div>
         </div>
-        <hr className='line'/>
+        <hr className={styles['line']}/>
         </>
     )
 }

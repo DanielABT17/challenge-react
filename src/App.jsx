c943ad0b9d5fd779acfdc37539c1ecfe2cd8821a
Tomas Gonzalez <tgonzalez@abatechnology.com>
2025-04-07 09:21:50 -0500

    Update App.jsx
    conflicts resolve

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import TopBar from "./components/top-bar/TopBar";
 import "./styles/defaultStyles.css";
-import ProductDetail from "./views/ProductDetail.jsx";
 import MenuShoes from "./views/menu-shoes/MenuShoes.jsx";
-import './styles/defaultStyles.css'
-import ProductDetail from './views/ProductDetail/ProductDetail.jsx'
 
 function App() {
   return (
